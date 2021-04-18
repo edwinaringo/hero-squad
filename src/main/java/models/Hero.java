dd1ea26677c545850a3ab27849e6a755765906ff
edwinaringo <tedaringo628@gmail.com>
2021-04-19 01:56:06 +0300

    Generating a test to check intantiation of Heroes

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,16 @@
 package models;
 
 public class Hero {
+    private String name;
+    private int age;
+    private String specialPower;
+    private String weakness;
+
+    public Hero(String name, int age, String specialPower, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.specialPower = specialPower;
+        this.weakness = weakness;
+    }
+
 }
--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,6 +10,16 @@ class HeroTest {
 
     @BeforeEach
     void setUp() {
+    }
+
+    @Test
+    void heroInstantiatesCorrectly() {
+        Hero newHero = setupNewHero();
+        assertEquals(true,newHero instanceof Hero);
+    }
+
+    private Hero setupNewHero() {
+        return new Hero ("Superman",24,"Flying","Kryptonite");
     }
 
     @AfterEach
