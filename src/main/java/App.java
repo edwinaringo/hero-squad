ddcadea91b3c1c877a2171e540c46d498f9331c9
edwinaringo <tedaringo628@gmail.com>
2021-04-20 22:29:58 +0300

    Creating the root route(index.hbs) on App.java file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,11 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 
 }
