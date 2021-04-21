5172949f27e4962d4e54cd175f87a4325695f4c7
edwinaringo <tedaringo628@gmail.com>
2021-04-21 15:10:09 +0300

    Adding logic onto our Squad class i.e Squad.java

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -5,10 +5,29 @@ import java.util.List;
 
 public class Squad {
     private String squadName;
-    private String squadCause;
+    private Hero newHero;
+    private String cause;
     private List<Object> heroMembers = new ArrayList<>();
     private static boolean isRegisteredHEro = false;
     private static List<Squad> squadList = new ArrayList<>();
     private int squadId;
+
+    public Squad(String name, String cause) {
+        this.squadName = name;
+        this.cause = cause;
+    }
+
+    public Hero getHero() {
+        return newHero;
+    }
+
+    public String getName() {
+        return squadName;
+    }
+
+
+    public int getSquadId() {
+        return squadId;
+    }
 
 }
